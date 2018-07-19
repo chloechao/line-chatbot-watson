dcf3c99fabd1e4fa5024a218d1d561644e668d37
Chloe Chao <chloechao@indeed.com>
2018-07-19 19:37:45 +0900

    use conversation id

--- api_call.py
+++ api_call.py
@@ -15,11 +15,14 @@ class IbmAssistant:
         version='2018-07-10')
 
     workspace_id = '1ef0a9c1-f07e-4bb7-adfc-0d0f3fb6ff4a'
+    conversation_id = None
 
     def list_workspace(self):
         return self.assistant.list_workspaces()
 
     def message_request(self, message):
+        print('CONVER_ID-init')
+        print(self.conversation_id)
         response = self.assistant.message(
             workspace_id=self.workspace_id,
             input={
@@ -27,9 +30,13 @@ class IbmAssistant:
             },
             alternate_intents=True,
             context={
+                'conversation_id': self.conversation_id,
                 'metadata': {
                     'deployment': 'myDeployment'
                 }
             })
         print(response)
+        self.conversation_id = response.get('context').get('conversation_id')
+        print('CONVER ID:')
+        print(self.conversation_id)
         return response.get('output').get('generic')[0].get('text')
